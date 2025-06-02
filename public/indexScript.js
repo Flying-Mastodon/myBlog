4ddc025b3609935ab3b692809eae1defd2c1c276
Dean Booker <deanpbooker@gmail.comgit config --global user.name Dean>
2025-06-02 11:45:03 +0100

    Branded to YTREWQ, updated home page to strip out writing to DB

---
+++ public/indexScript.js
@@ -0,0 +1,38 @@
+const API_BASE_URL = 'https://myblog-sy0j.onrender.com'; 
+
+
+const postsContainer = document.getElementById('posts-container');
+
+async function loadPosts() {
+    try {
+      const res = await fetch(`${API_BASE_URL}/posts`);
+      if (!res.ok) throw new Error('Failed to fetch posts');
+      const posts = await res.json();
+
+      postsContainer.innerHTML = '';
+      posts.forEach(post => {
+        const formattedDate = formatUKDate(post.created_at);
+        const el = document.createElement('div');
+        el.className = 'post';
+        el.innerHTML = `${formattedDate}<h3>${post.title}</h3><p>${post.content}</p>`;
+        postsContainer.appendChild(el);
+      });
+    } catch (error) {
+      postsContainer.textContent = 'Error loading posts.';
+      console.error(error);
+    }
+  }
+
+loadPosts();
+
+
+
+function formatUKDate(isoString) {
+  const date = new Date(isoString);
+  return date.toLocaleDateString('en-GB', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  });
+}
+
