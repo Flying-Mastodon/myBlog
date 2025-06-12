fa5a989e902b9f841b0842948c4ba69ebdbcc122
Dean Booker <deanpbooker@gmail.comgit config --global user.name Dean>
2025-06-12 17:15:04 +0100

    Hotfix, JWT is back baby

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { createClient } = require('@supabase/supabase-js');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 3000;
